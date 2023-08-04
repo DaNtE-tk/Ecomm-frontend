dc6f5776132bd52d4589f29f1f954739611e74b8
Tushar Kashyap <tusharkasyap2342@gmail.com>
2023-08-04 23:16:41 +0530

    error detected

--- src/pages/CartScreen.js
+++ src/pages/CartScreen.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Link, useParams } from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
+import {Message} from '../components/Message'
+import {addToCart} from '../actions/cartActions'
 
-function CartScreen() {
+
+function CartScreen({match, location, history}) {
+  const {id} = useParams();
+  const {qty} = location.search
+  console.log('qty: ',qty);
   return (
     <div>
       CART
